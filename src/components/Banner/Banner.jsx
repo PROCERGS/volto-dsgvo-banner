f3e2325c76a19568821201f48f84bdcdfc02717a
Andressa Sias <andressa-sias@procergs.rs.gov.br>
2026-03-05 13:05:54 -0300

    #1336092 tags semânticas

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -162,20 +162,20 @@ const Banner = (props) => {
                   defaultMessage="To give you the best possible user experience, we use cookies on our website. Cookies required for the operation of the site are set automatically. You can also allow other cookies. You can {agree_all_cookies} or {adjust_privacy_settings}."
                   values={{
                     agree_all_cookies: (
-                      <span class="strong">
+                      <strong>
                         <FormattedMessage
                           id="agree to all cookies"
                           defaultMessage="agree to all cookies"
                         />
-                      </span>
+                      </strong>
                     ),
                     adjust_privacy_settings: (
-                      <span class="strong">
+                      <strong>
                         <FormattedMessage
                           id="adjust the privacy settings"
                           defaultMessage="adjust the privacy settings"
                         />
-                      </span>
+                      </strong>
                     ),
                   }}
                 />
@@ -185,14 +185,14 @@ const Banner = (props) => {
                   id="You can find more information on the used cookies and how you can subsequently revoke your consent in our"
                   defaultMessage="You can find more information on the used cookies and how you can subsequently revoke your consent in our"
                 />{' '}
-                <span class="strong">
+                <strong>
                   <Link to={privacy_url} onClick={confirmSelection}>
                     <FormattedMessage
                       id="Privacy Policy"
                       defaultMessage="Privacy Policy"
                     />
                   </Link>
-                </span>
+                </strong>
                 .
               </p>
             </Modal.Content>
